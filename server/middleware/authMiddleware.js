81dd936c1f702f857bcf31537a06d9ea48c33613
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-10 18:15:14 +0530

    space is removed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,27 +1,25 @@
-import jwt from 'jsonwebtoken';
-import Company from '../models/Company.js';
+import jwt from "jsonwebtoken";
+import Company from "../models/Company.js";
 
 export const protectCompany = async (req, res, next) => {
+  const token = req.headers.token;
 
-    const token = req.headers.token 
+  if (!token) {
+    return res.json({
+      success: false,
+      message: "Not authorized",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if(!token){
-        return res.json({
-            success: false,
-            message: "Not authorized"})
-    }
-    try {
-        
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.company = await Company.findById(decoded.id).select("-password");
 
-        req.company =  await Company.findById(decoded.id).select('-password')
-
-        next()
-
-    } catch (error) {
-        res.json({
-            success: false,
-            message: "Not authorized, Login Again"})
-    }
-
-}
+    next();
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Not authorized, Login Again",
+    });
+  }
+};
