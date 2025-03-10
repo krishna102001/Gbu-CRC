ef6af452df588b214933e3840fe4ab4ef9df6e4f
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-10 18:14:36 +0530

    fix:middleware-logic

--- server/middleware/adminMiddleware.js
+++ server/middleware/adminMiddleware.js
@@ -1,9 +1,17 @@
 import jwt from "jsonwebtoken";
 const adminMiddleWare = async (req, res, next) => {
-  const token = req.headers.authorization;
+  let token = req.headers.authorization;
   if (!token) {
-    return res.status(402).json({ success: false, message: "Token Not Found" });
+    return res.status(401).json({ success: false, message: "Token Not Found" });
   }
+  console.log(token);
+  if (!token.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "token is invalid" });
+  }
+
+  token = token.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.adminId = decoded.adminId;
