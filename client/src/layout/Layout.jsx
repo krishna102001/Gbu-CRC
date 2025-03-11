23520f253c4cb3d865e41709214837325aa9cf31
krishna102001 <krishnakantmaurya@icloud.com>
2025-03-11 23:38:19 +0530

    MAked Wrapper Component for Admin Dashboard

---
+++ client/src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import AdminNavbar from "../components/Admin/AdminNavbar";
+import AdminFooter from "../components/Admin/AdminFooter";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="<div className='flex flex-col min-h-screen'>">
+      <AdminNavbar />
+      <div className=' flex-1'>{children}</div>
+      <AdminFooter />
+    </div>
+  );
+};
+
+export default Layout;
