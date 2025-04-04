3056a23234f6581df8c31f8e3a7f1e4dd3555d7a
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-04 21:56:08 +0530

    fix:[added phone no & resume relationship defined]

--- server/models/Company.js
+++ server/models/Company.js
@@ -5,6 +5,7 @@ const companySchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   image: { type: String, required: true },
+  phone: { type: String, required: true },
 });
 
 const Company = mongoose.model("Company", companySchema);
