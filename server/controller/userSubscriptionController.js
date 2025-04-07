bbecbcac7afdbb1210644035df39472ae8edb51f
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-07 18:47:29 +0530

    email subscribed notification done

---
+++ server/controller/userSubscriptionController.js
@@ -0,0 +1,20 @@
+import Subscription from "../models/Subscriptions.js";
+
+export const emailSubscribe = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const data = await Subscription.findOne({ email });
+    if (data) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Already Subscribed" });
+    }
+    await Subscription.create({
+      email,
+    });
+    res.status(200).json({ success: true, message: "Successfully Subscribed" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Failed Try again" });
+  }
+  console.log(email);
+};
--- server/routes/userSubscriptionRoutes.js
+++ server/routes/userSubscriptionRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { emailSubscribe } from "../controller/userSubscriptionController.js";
 
 const router = express.Router();
+
+router.post("/subscribe", emailSubscribe);
 
 export default router;
